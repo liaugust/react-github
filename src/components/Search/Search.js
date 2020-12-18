58f2d64705ea44fde993eafae8fceb37940264b8
liaugust <august.ivan228@gmail.com>
2020-12-18 20:15:38 +0200

    Implemented github profile searching

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,18 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+
 import { AlertContext } from "../../context/alert/alertContext";
+import { GithubContext } from "../../context/github/githubContext";
 
 export const Search = () => {
-  const {show} = useContext(AlertContext)
+  const [value, setValue] = useState('')
+  const alert  = useContext(AlertContext)
+  const github = useContext(GithubContext)
 
   const onSubmit = event => {
-    if (event.key === 'Enter') {
-      show('This is alert')
+    if (event.key !== 'Enter') return
+
+    if (value.trim()) {
+      github.search(value.trim())
+      alert.hide()
+    } else {
+      alert.show('Введите данные пользователя!')
+      github.clearUsers()
     }
   }
 
   return (
     <div className='formGroup'>
-      <input type='text' className='form-control mb-4' placeholder='Введите ник пользователя...' onKeyPress={onSubmit}/>
+      <input
+        value={value}
+        type='text'
+        className='form-control mb-4'
+        placeholder='Введите ник пользователя...'
+        onChange={event => setValue(event.target.value)}
+        onKeyPress={onSubmit}
+      />
     </div>
   )
 }
